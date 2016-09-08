cfe27da02eb8f35d423ac94dcbf0c2ed14673809
David Vespoli <david.vespoli@gmail.com>
2016-09-07 20:18:54 -0700

    cleanup

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var cron = require('./data/cron');
+//var cron = require('./data/cron');
 
 var app = express();
 var precog = require('./data/precog');
@@ -22,12 +22,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.all('/api/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -72,7 +72,7 @@ app.use(function(err, req, res, next) {
 });
 
 //start data cron job
-cron.job.start();
+//cron.job.start();
 
 //start precog to detect potential crime
 precog.run();
