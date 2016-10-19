75fd36d5579b9bdbcec3057a4f79b162f2de8c35
adpace <adpace@hotmail.com>
2016-10-19 09:13:20 -0700

    added underline to address

--- public/js/main.js
+++ public/js/main.js
@@ -14429,6 +14429,7 @@ niApp.controller('NIController', function NIController($scope, $window, $http, N
   var getData = function(){
     if($scope.user.declinedLocation){return;}
     $scope.loading = true;
+    console.log("sending hm request for " + $scope.user.lat + "," + $scope.user.lng );
     $http({
       url: apiUrl + '/' + $scope.user.lat + '/' + $scope.user.lng,
       method: "GET",
@@ -14439,6 +14440,7 @@ niApp.controller('NIController', function NIController($scope, $window, $http, N
       $scope.riskText = results.data.precog.time[hour].risk;
       $scope.riskLevel = results.data.precog.time[hour].risk.toLowerCase();
       $scope.mostLikely = results.data.precog.time[hour].guess;
+      console.log("result: " + $scope.riskLevel + "," + $scope.mostlikely );
       $scope.loading = false;
       $scope.init = true;
     });
@@ -14549,6 +14551,7 @@ niApp.controller('MoreController', function MoreController($scope, $window, $htt
 
   var getData = function(){
     $scope.loading = true;
+    console.log("sending hm request for " + $scope.user.lat + "," + $scope.user.lng );
     $http({
       url: apiUrl + '/' + $scope.user.lat + '/' + $scope.user.lng,
       method: "GET",
@@ -14565,6 +14568,7 @@ niApp.controller('MoreController', function MoreController($scope, $window, $htt
       $scope.highestTime = $scope.timesOfDay[indexOfMax(results.data.precog.timeOfDay)];
       $scope.highestDayData = results.data.precog.dayOfWeek;
       $scope.highestDay = $scope.daysOfWeek[indexOfMax(results.data.precog.dayOfWeek)];
+      console.log("result: " + $scope.highestTime + "," + $scope.highestDay );
     });
   };
 
